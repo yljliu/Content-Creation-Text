13baab50c489aa1cf0e92e4b1106371f8e44f816
yljliu <112994910+yljliu@users.noreply.github.com>
2025-06-08 20:39:02 -0700

    Create googleAPI.py

---
+++ googleAPI.py
@@ -0,0 +1,72 @@
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import pandas as pd
+
+# # If modifying these scopes, delete the file token.json.
+# SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+
+# The ID and range of a sample spreadsheet.
+# SAMPLE_SPREADSHEET_ID = "1SlXnyv41JkCJy1g2zJzbbJ0zW4tpfYxnAQY9_TXjDcA"
+# SAMPLE_RANGE_NAME = "Destination!A1:Z"
+
+class Sheet:
+
+  SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+  SAMPLE_SPREADSHEET_ID = "1SlXnyv41JkCJy1g2zJzbbJ0zW4tpfYxnAQY9_TXjDcA"
+
+  def __init__(self, range):
+    self.range = range
+
+  def read_sheet(self):
+
+    """Shows basic usage of the Sheets API. Prints values from a sample spreadsheet."""
+    creds = None
+
+    # The file token.json stores the user's access and refresh tokens, and is created automatically when the authorization flow completes for the first time.
+    if os.path.exists("token.json"):
+      creds = Credentials.from_authorized_user_file("token.json", self.SCOPES)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+      if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+      else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            "credentials.json", self.SCOPES
+        )
+        creds = flow.run_local_server(port=0)
+      # Save the credentials for the next run
+      with open("token.json", "w") as token:
+        token.write(creds.to_json())
+
+    try:
+      service = build("sheets", "v4", credentials=creds)
+
+      # Call the Sheets API
+      sheet = service.spreadsheets()
+      result = (
+          sheet.values()
+          .get(spreadsheetId = self.SAMPLE_SPREADSHEET_ID, range = self.range)
+          .execute()
+      )
+
+      values = result.get("values", [])
+
+      if not values:
+        print("No data found.")
+        return
+
+      '''Set first row to column headers'''
+      header = values.pop(0)
+      values_df = pd.DataFrame(values)
+      values_df.columns = header
+      
+      return values_df 
+    except HttpError as err:
+      print(err)
+      
+      return 
